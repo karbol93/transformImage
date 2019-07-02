308fa92324663c76c07d6cfd815f918b901b2a53
karbol <lukaszkarbowiak93@gmail.com>
2019-07-02 19:38:27 +0000

    first working version. To execute run /build/transformImage.out

---
+++ build/CMakeFiles/transformImage.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transformImage.out.dir/transformImage.cpp.o"
+  "transformImage.out.pdb"
+  "transformImage.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transformImage.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ transformImage.cpp
@@ -0,0 +1,25 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+ 
+int main( int argc, char** argv )
+{
+  cv::Mat image;
+  image = cv::imread("../example_image.jpg");
+  unsigned imgSize = image.rows * image.cols * 3; // 3= RGB, 1= Monochromatic
+   
+  while ( ! image.isContinuous() )
+  { 
+      image = image.clone();
+  }
+  
+  uint32_t * imageData = (uint32_t*)image.data;
+  
+  for( unsigned i = 0; i < imgSize; i = i + 4)
+  {
+      *imageData++ -= 3368601800; //image - 200
+  }
+  
+  cv::imwrite("../transformed_image.jpg",image);
+
+  return 0;
+}
