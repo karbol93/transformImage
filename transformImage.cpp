b1a788b39bf959078cd39c1c63b14cac305229a6
karbol <lukaszkarbowiak93@gmail.com>
2019-07-04 16:13:58 +0000

    deleted magic number. Calculate value for 4 pixels

--- transformImage.cpp
+++ transformImage.cpp
@@ -1,10 +1,25 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
- 
+#include <climits>
+
+unsigned changeValue(unsigned value)
+{ 
+    value = value & UCHAR_MAX; //pixel value 0-255
+    unsigned temp = value;
+    for(unsigned i = 0; i < 3; i++)
+    {
+        temp = temp << 8;
+        temp = temp | value;
+    }
+    return temp;
+}
+
 int main( int argc, char** argv )
 {
   cv::Mat image;
   image = cv::imread("../example_image.jpg");
+  unsigned value = 200;
+  value = changeValue(value);
   unsigned imgSize = image.rows * image.cols * image.channels(); 
 
   while ( ! image.isContinuous() )
@@ -16,7 +31,7 @@ int main( int argc, char** argv )
   
   for( unsigned i = 0; i < imgSize; i = i + 4)
   {
-      *imageData++ -= 3368601800; //image - 200
+      *imageData++ -= value; 
   }
   
   cv::imwrite("../transformed_image.jpg",image);
